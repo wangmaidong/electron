6d14e62ae842147b4d181040e8c5f8dae6fdab79
wanglei <2934490605@qq.com>
2023-04-20 11:12:53 +0800

    feat: 应用协议唤起

--- src/main/index.js
+++ src/main/index.js
@@ -1,12 +1,50 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-let win
-app.whenReady().then(() => {
-  win = new BrowserWindow({
-    width: 800,
-    height: 600,
-    titleBarStyle: 'hiddenInset'
+const { app, BrowserWindow } = require('electron')
+
+let mainWindow
+
+const protocol = 'juejin'
+const scheme = `${protocol}://`
+app.setAsDefaultProtocolClient(protocol)
+
+let urlParams = {}
+
+handleSchemeWakeup(process.argv)
+
+const gotTheLock = app.requestSingleInstanceLock()
+if (!gotTheLock) {
+  app.quit()
+} else {
+  app.on('second-instance', (event, argv) => {
+    mainWindow.restore()
+    mainWindow.show()
+    handleSchemeWakeup(argv)
   })
-  win.loadFile(path.join(__dirname, '../renderer/index.html'))
-  win.webContents.openDevTools()
+}
+
+app.on('open-url', (event, url) => handleSchemeWakeup(url))
+
+app.whenReady().then(() => {
+  createWindow()
 })
+
+function createWindow() {
+  const width = parseInt(urlParams.width) || 800
+  const height = parseInt(urlParams.height) || 600
+  const loadURL = urlParams.url || 'https://www.juejin.cn'
+  console.log()
+  if (mainWindow) {
+    mainWindow.setSize(width, height)
+  } else {
+    mainWindow = new BrowserWindow({ width, height })
+    mainWindow.loadURL(loadURL)
+    mainWindow.webContents.openDevTools()
+  }
+}
+
+function handleSchemeWakeup(argv) {
+  const url = [].concat(argv).find((v) => v.startsWith(scheme))
+  if (!url) return
+  const searchParams = new URLSearchParams(url.slice(scheme.length))
+  urlParams = Object.fromEntries(searchParams.entries())
+  if (app.isReady()) createWindow()
+}
